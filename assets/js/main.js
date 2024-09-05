3c86e7f5f62285e71cbbcab7b2a09bb08c66ae28
haiisol <faisal.isol@gmail.com>
2024-09-06 06:29:38 +0700

    updated version

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,7 +71,7 @@
             arrows: false,
             speed: 500,
             slidesToShow: 3,
-            slidesToScroll: 1,
+            slidesToScroll: 3,
             autoplay: false,
             autoplaySpeed: 5000,
             responsive: [
