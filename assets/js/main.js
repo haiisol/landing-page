286029e78bc1f9d49bcb867c52b5dab9ca53f143
Faisal <faisal@twmg.com.au>
2024-09-06 13:18:42 +0700

    updated version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,67 +1,6 @@
 (function ($) {
     'use strict';
 
-    // ===== Main Menu
-    function mainMenu() {
-        const navbarToggler = $('.navbar-toggler'),
-            navMenu = $('.nav-menu'),
-            mobilePanel = $('.mobile-slide-panel'),
-            mobilePanelMenu = $('.mobile-menu'),
-            mobileOverly = $('.panel-overlay'),
-            navClose = $('.panel-close');
-
-        // Show Mobile Slide Panel
-        navbarToggler.on('click', function (e) {
-            e.preventDefault();
-            mobilePanel.toggleClass('panel-on');
-        });
-
-        // Close Mobile Slide Panel
-        navClose.add(mobileOverly).on('click', function (e) {
-            e.preventDefault();
-            mobilePanel.removeClass('panel-on');
-        });
-
-        // Adds toggle button to li items that have children
-        navMenu.find('li a').each(function () {
-            if ($(this).next().length > 0) {
-                $(this).append('<span class="dd-trigger"><i class="fas fa-angle-down"></i></span>');
-            }
-        });
-
-        mobilePanelMenu.find('li a').each(function () {
-            if ($(this).next().length > 0) {
-                $(this).append('<span class="dd-trigger"><i class="fas fa-angle-down"></i></span>');
-            }
-        });
-
-        // Expands the dropdown menu on each click
-        mobilePanelMenu.find('.dd-trigger').on('click', function (e) {
-            e.preventDefault();
-            $(this).parent().parent('li').children('ul.sub-menu').stop(true, true).slideToggle(350);
-            $(this).toggleClass('sub-menu-opened');
-        });
-
-
-        const offCanvasBtn = $('.off-canvas-btn'),
-            offCanvasClose = $('.canvas-close'),
-            canvasOverly = $('.canvas-overlay'),
-            offCanvasPanel = $('.off-canvas-wrapper');
-
-        // Show Off canvas Panel
-        offCanvasBtn.on('click', function (e) {
-            e.preventDefault();
-            offCanvasPanel.addClass('canvas-on');
-        });
-
-        // Hide Off canvas Panel
-        offCanvasClose.add(canvasOverly).on('click', function (e) {
-            e.preventDefault();
-            offCanvasPanel.removeClass('canvas-on');
-        });
-    }
-
-    // ===== Testimonials Slider One
     function testimonialActiveOne() {
         const slider = $('#testimonialActiveOne');
 
@@ -105,32 +44,6 @@
         }
     }
 
-    // ===== Scroll To Top
-    function scrollToTop() {
-        var $scrollUp = $('#scroll-top'),
-            $lastScrollTop = 0,
-            $window = $(window);
-
-        $window.on('scroll', function () {
-            var st = $(this).scrollTop();
-            if (st > $lastScrollTop) {
-                $scrollUp.removeClass('show');
-            } else {
-                if ($window.scrollTop() > 200) {
-                    $scrollUp.addClass('show');
-                } else {
-                    $scrollUp.removeClass('show');
-                }
-            }
-            $lastScrollTop = st;
-        });
-
-        $scrollUp.on('click', function (evt) {
-            $('html, body').animate({ scrollTop: 0 }, 600);
-            evt.preventDefault();
-        });
-    }
-
     function deferBG() {
         var imgDefer = document.querySelectorAll('.defer-bg[data-src]');
         var style = "background-image: url({url})";
@@ -143,9 +56,7 @@
     === Document Ready  ===
     ----------------------*/
     $(document).ready(function () {
-        mainMenu();
         testimonialActiveOne();
-        scrollToTop();
         deferBG()
     });
 
